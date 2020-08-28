ab1c9a00674cd485822522ec895d3b5e0db32792
Claire Harris <wishing.engine@gmail.com>
2020-08-29 00:19:02 +0100

    Move shader compilation out of build script to manual command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::path::PathBuf;
 
 use image::{DynamicImage, RgbaImage};
+
+#[cfg(feature = "cli")]
 use structopt::StructOpt;
 
 type DepthImage = image::ImageBuffer<image::Luma<u16>, Vec<u16>>;
 
+#[cfg(feature = "cli")]
 #[derive(Debug, StructOpt)]
 enum Command {
+    Shaders,
     ToDepth {
         in_file: PathBuf,
 
@@ -21,6 +25,12 @@ enum Command {
     },
 }
 
+#[cfg(not(feature = "cli"))]
+fn main() {
+    panic!("Not compiled with CLI support!");
+}
+
+#[cfg(feature = "cli")]
 fn main() {
     let opt = Command::from_args();
     match opt {
@@ -63,5 +73,66 @@ fn main() {
                 stdout.write_all(&color).unwrap();
             }
         }
+        Command::Shaders => {
+            compile_shaders().unwrap();
+        }
     }
+}
+
+#[cfg(feature = "cli")]
+fn compile_shaders() -> Result<(), Box<dyn std::error::Error>> {
+    let mut compiler = shaderc::Compiler::new().unwrap();
+    let options = shaderc::CompileOptions::new().unwrap();
+
+    // Create destination path if necessary
+    std::fs::create_dir_all("assets/compiled")?;
+
+    for entry in std::fs::read_dir("assets")? {
+        let entry = entry?;
+
+        if entry.file_type()?.is_file() {
+            let in_path = entry.path();
+
+            // Support only vertex and fragment shaders currently
+            let shader_type =
+                in_path
+                    .extension()
+                    .and_then(|ext| match ext.to_string_lossy().as_ref() {
+                        "vert" => Some(shaderc::ShaderKind::Vertex),
+                        "frag" => Some(shaderc::ShaderKind::Fragment),
+                        _ => None,
+                    });
+
+            if let Some(shader_type) = shader_type {
+                let file_name = in_path.file_name().unwrap().to_string_lossy();
+
+                let source = std::fs::read_to_string(&in_path)?;
+                let binary_result = compiler.compile_into_spirv(
+                    &source,
+                    shader_type,
+                    &file_name,
+                    "main",
+                    Some(&options),
+                );
+
+                let compiled_bytes = match binary_result {
+                    Err(s) => {
+                        eprintln!("\nFAILED to compile: {}\n", in_path.display());
+                        eprintln!("{}", s);
+                        panic!("Shader compilation failed.");
+                    }
+                    Ok(result) => result,
+                };
+
+                let out_path = format!(
+                    "assets/compiled/{}.spv",
+                    in_path.file_name().unwrap().to_string_lossy()
+                );
+
+                std::fs::write(&out_path, compiled_bytes.as_binary_u8())?;
+            }
+        }
+    }
+
+    Ok(())
 }
